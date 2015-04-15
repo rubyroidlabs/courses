077f335483ecf285aebac58c842013548d8874bb
Yury Kaliada <fut.wrk@gmail.com>
2015-04-15 15:24:48 +0300

    split example over multiple lines
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- materials/0 - Ruby vs C/winner.rb
+++ materials/0 - Ruby vs C/winner.rb
@@ -1,1 +1,8 @@
-(1..40).to_a.shuffle.select{|i| i % 2 == (Time.now.to_i.odd? ? 0 : 1) }.shuffle.select.with_index{|num, i| i % 2 == (Time.now.to_i.odd? ? 0 : 1)}.shuffle.sample
+puts (1..40).
+  to_a.
+  shuffle.
+  select{|i| i % 2 == (Time.now.to_i.odd? ? 0 : 1) }.
+  shuffle.
+  select.with_index{|num, i| i % 2 == (Time.now.to_i.odd? ? 0 : 1)}.
+  shuffle.
+  sample
